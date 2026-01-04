69a360d49146931829cb5c478de5a25ba0371b24
Adrian <adriantimothypetersson@gmail.com>
2026-01-04 11:04:52 +0100

    minor fixes

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -2,17 +2,21 @@ import { Loading03Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { cn } from "@/lib/utils";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
-	return (
-		<HugeiconsIcon
-			icon={Loading03Icon}
-			strokeWidth={2}
-			role="status"
-			aria-label="Loading"
-			className={cn("size-4 animate-spin", className)}
-			{...props}
-		/>
-	);
+function Spinner({
+  className,
+  strokeWidth = 2,
+  ...props
+}: React.ComponentProps<"svg">) {
+  return (
+    <HugeiconsIcon
+      icon={Loading03Icon}
+      strokeWidth={Number(strokeWidth)}
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
+  );
 }
 
 export { Spinner };
