1a98223dd412e9e841983d4bb5322536177e26f4
Adrian <adriantimothypetersson@gmail.com>
2026-01-07 22:02:16 +0100

    create auth schema

--- db/db.ts
+++ db/db.ts
@@ -1,10 +1,11 @@
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as authSchema from "./auth-schema";
 import * as schema from "./schema";
 
 config({ path: ".env.local" });
 
 const sql = neon(process.env.DATABASE_URL ?? "");
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema: { ...schema, ...authSchema } });
