1a32c8a9ee17ef3329c1387a4543633fb0c94c5d
Adrian <adriantimothypetersson@gmail.com>
2026-01-08 18:55:28 +0100

    add sign-in

---
+++ app/trips/page.tsx
@@ -0,0 +1,3 @@
+export default function TripDashboardPage() {
+	return <div>Trip Dashboard Home Page</div>;
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { signIn } from "@/lib/auth-client";
 import { Button } from "./ui/button";
 
 export const Header = () => {
+	const onSignInClick = async () => {
+		const { data, error } = await signIn.social({
+			provider: "google",
+
+			callbackURL: "/trips",
+			errorCallbackURL: "/error",
+		});
+		console.log("Sign-in data:", data, "Error:", error);
+	};
+
 	return (
 		<header className="flex h-16 w-full items-center border-b px-2 lg:px-10">
 			<nav className="flex w-full items-center justify-between">
 				<span>Aeropack</span>
-				<div>
-					<Button
-						size="lg"
-						render={
-							<Link href="/login" className="mr-4">
-								Login
-							</Link>
-						}
-					/>
+				<div className="space-x-4">
+					<Button size="lg" onClick={onSignInClick}>
+						Sign In
+					</Button>
 					<Link href="/signup" className="btn-primary">
 						Sign Up
 					</Link>
